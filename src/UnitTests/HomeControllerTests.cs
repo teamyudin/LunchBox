cb93fa5f3b72a810dbf84fdab16674274716e852
team.yudin <team.yudin@gmail.com>
2012-06-03 15:08:48 -0500

    added UI and UnitTests projects

---
+++ src/UnitTests/HomeControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
